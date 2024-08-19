a41ee89529af2d9b792e74c17247e8797b153850
TEJA RAM <135523042+tejjanee@users.noreply.github.com>
2024-08-19 13:31:36 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   const handleLogout = async () => {
     await axios
-      .get("https://dep-back-s4j5.onrender.com/api/v1/user/patient/logout", {
+      .get("https://back-dep.onrender.com/api/v1/user/patient/logout", {
         withCredentials: true,
       })
       .then((res) => {
