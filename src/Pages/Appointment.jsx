dcb75af4ca91e1a1fe058c4dd14b67c3c690bed0
TEJA RAM <135523042+tejjanee@users.noreply.github.com>
2024-08-19 13:30:06 +0530

    Update Appointment.jsx

--- src/Pages/Appointment.jsx
+++ src/Pages/Appointment.jsx
@@ -34,7 +34,7 @@ const AppointmentForm = () => {
   useEffect(() => {
     const fetchDoctors = async () => {
       const { data } = await axios.get(
-        "https://dep-back-s4j5.onrender.com/api/v1/user/doctors",
+        "https://back-dep.onrender.com/api/v1/user/doctors",
         { withCredentials: true }
       );
       setDoctors(data.doctors);
@@ -47,7 +47,7 @@ const AppointmentForm = () => {
     try {
       const hasVisitedBool = Boolean(hasVisited);
       const { data } = await axios.post(
-        "https://dep-back-s4j5.onrender.com/api/v1/appointment/post",
+        "https://back-dep.onrender.com/api/v1/appointment/post",
         {
           firstName,
           lastName,
